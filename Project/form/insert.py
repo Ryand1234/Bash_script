86d9a8214559116cc570fd563468cdf77fee635a
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-02-12 18:50:35 +0530

    form_web_app

--- Project/form/insert.py
+++ Project/form/insert.py
@@ -7,21 +7,15 @@ contry = str(sys.argv[4])
 password = str(sys.argv[5])
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="riyan1234",  # your password
-                     db="data")        # name of the data base
-
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
+db = MySQLdb.connect(host="localhost",   
+                     user="root",        
+                     passwd="riyan1234",  
+                     db="data")        
 cur = db.cursor()
-
-# Use all the SQL you like
 query = "INSERT INTO form (Name,Number,Email,Address,Password) values (%s,%s,%s,%s,%s)"
 arg = (name,mobile,email,contry,password)
 cur.execute(query,arg)
 db.commit()
-# print all the first cell of all the rows
 '''for row in cur.fetchall():
     for i in range(0,4):
     	print (str(row[i])+ " ")
