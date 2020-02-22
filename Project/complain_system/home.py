eeae1d0f512b62f845e595d49ce9d2bfa9abe8b6
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-02-22 20:52:00 +0530

    update

--- Project/complain_system/home.py
+++ Project/complain_system/home.py
@@ -21,10 +21,9 @@ def detail():
 			k = check_output(["python3","strip_complain.py",message])
 			k = k.decode('utf-8')
 			k=str(k[0])
+			check_output(["sudo","python3","to_database.py",name,mob,email,k,bank,message])
 			return render_template("complaining.html")
 		else:
-			wait()
-			check_output(["sudo","python3","to_database.py",name,mob,email,k,bank,message])
 			return render_template("complain_filed.html")	
 if __name__ == "__main__":
 	app.run(debug=True,host="localhost")
